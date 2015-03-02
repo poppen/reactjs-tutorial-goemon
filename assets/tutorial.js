4e5bd65e0714b4dba0cf47e811acd7261acf9af9
poppen <poppen.jp@gmail.com>
2015-03-02 10:28:44 +0900

    add CommentList and CommentForm

--- assets/tutorial.js
+++ assets/tutorial.js
@@ -1,8 +1,30 @@
+var CommentList = React.createClass({
+	render: function() {
+		return (
+				<div className="CommentList">
+					Hello, world! I am CommentList.
+				</div>
+			   );
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function() {
+			return (
+				<div className="CommentForm">
+					Hello, world! I am a CommentForm
+				</div>
+				);
+	}
+});
+
 var CommentBox = React.createClass({
 	render: function() {
 		return (
 				<div className="CommentBox">
-					Hello, world! I am CommentBox.
+					<h1>Hello, world! I am CommentBox.</h1>
+					<CommentList />
+					<CommentForm />
 				</div>
 			   );
 	}
