e30b544ae5a64f004cea1aad954185dba9d598d5
poppen <poppen.jp@gmail.com>
2015-03-03 14:19:43 +0900

    implement POST

--- assets/tutorial.js
+++ assets/tutorial.js
@@ -31,10 +31,24 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var author= this.refs.author.getDOMNode().value.trim();
+		var text = this.refs.text.getDOMNode().value.trim();
+		if (!text || !author) {
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		this.refs.author.getDOMNode().value = '';
+		this.refs.text.getDOMNode().value = '';
+	},
 	render: function() {
 			return (
-				<div className="CommentForm">
-				</div>
+				<form className="commentForm" onSubmit={this.handleSubmit}>
+					<input type="text" placeholder="Your name" ref="author" />
+					<input type="text" placeholder="Say somithing..." ref="text" />
+					<input type="submit" value="Post" />
+				</form>
 				);
 	}
 });
@@ -55,6 +69,20 @@ var CommentBox = React.createClass({
 	getInitialState: function() {
 		return {data: []};
 	},
+	handleCommentSubmit: function(comment) {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.log(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
 	componentDidMount: function() {
 		this.loadCommentsFromServer();
 		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
@@ -64,7 +92,7 @@ var CommentBox = React.createClass({
 				<div className="CommentBox">
 					<h1>CommentBox</h1>
 					<CommentList data={this.state.data} />
-					<CommentForm />
+					<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 				</div>
 			   );
 	}
